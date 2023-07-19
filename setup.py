400819475a8c81bc2fd0aa77a652dcbeb9ec35ad
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-07-19 08:38:01 +0200

    add long description

--- setup.py
+++ setup.py
@@ -12,12 +12,20 @@ def requirements():
     with open('requirements.txt') as f:
         return f.read().splitlines()
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='hserv',
     version=version(),
     author='Mirko Mälicke',
     author_email='mirko@hydrocode.de',
     description='cli / api toolchain for managing hydrocode servers',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     install_requires=requirements(),
     packages=find_packages(),
 )
