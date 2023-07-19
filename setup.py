0fa1b2f9a1cc093bf550b5b9ab6cc08faa2d1828
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-07-19 08:19:29 +0200

    install from file

--- setup.py
+++ setup.py
@@ -8,12 +8,16 @@ def version():
         return loc['__version__']
 
 
+def requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
 setup(
     name='hserv',
     version=version(),
     author='Mirko Mälicke',
     author_email='mirko@hydrocode.de',
     description='cli / api toolchain for managing hydrocode servers',
-    install_requires=[],
+    install_requires=requirements(),
     packages=find_packages(),
 )
