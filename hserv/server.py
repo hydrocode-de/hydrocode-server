7e7c6a19d630a6169a2d1a7afd7a6ef50bee0859
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-07-20 17:09:48 +0200

    add put and get file methods

--- hserv/server.py
+++ hserv/server.py
@@ -1,6 +1,9 @@
-from typing import cast
+from typing import Union, Optional, cast
 from dataclasses import dataclass
 import re
+import io
+import os
+import shutil
 
 from fabric import Connection
 
@@ -26,6 +29,65 @@ class HydrocodeServer(object):
             return _run_local
         else:
             return _run
+
+    @property
+    def put(self):
+        # create a remote wrapper
+        def _put(*args, **kwargs):
+            with Connection(self.connection) as con:
+                return con.put(*args, **kwargs)
+        
+        def _put_local(local: Union[str, io.IOBase], remote: Optional[str] = None):
+            # check combination
+            if not isinstance(local, str) and remote is None:
+                raise ValueError("If local is not a string, remote must be specified.")
+            elif isinstance(local, str) and remote is None:
+                remote = local
+            
+            # check if the pass object is a string
+            if isinstance(local, str):
+                shutil.copyfile(local, cast(str, remote))
+            
+            # otherwise, check the type
+            with open(cast(str, remote), 'w' if isinstance(local, io.StringIO) else 'wb') as f:
+                cast(io.IOBase, local).seek(0)
+                f.write(cast(io.IOBase, local).read())
+        
+        # check which one to use
+        if 'localhost' in self.connection:
+            return _put_local
+        else:
+            return _put
+
+    @property
+    def get(self):
+        # create a remote wrapper
+        def _get(*args, **kwargs):
+            with Connection(self.connection) as con:
+                return con.get(*args, **kwargs)
+        
+        def _get_local(remote: str, local: Optional[Union[str, io.IOBase]] = None):
+            # if local is None, use the basename of remote
+            if local is None:
+                local = os.path.basename(remote)
+            
+            # if local is a string, copy the file
+            if isinstance(local, str):
+                shutil.copyfile(remote, local)
+            else:
+                # otherwise, read in the file as BytesIO
+                with open(remote, 'rb') as f:
+                    # check the buffer type
+                    if isinstance(local, io.StringIO):
+                        local.write(f.read().decode())
+                    else:
+                        local.write(f.read())
+
+        # check which one to use
+        if 'localhost' in self.connection:
+            return _get_local
+        else:
+            return _get                
 
     def _extract_semver(self, command: str) -> str:
         # get git version
