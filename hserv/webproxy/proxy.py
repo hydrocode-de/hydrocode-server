e54befbf8c638f91136b09ac94e9500171c491c6
Mirko Mälicke <mirko@hydrocode.de>
2023-07-25 07:07:25 +0200

    WebProxy ABC class

--- hserv/webproxy/proxy.py
+++ hserv/webproxy/proxy.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import abc
 from dataclasses import dataclass, field
 
@@ -5,4 +6,18 @@ from hserv.server import HydrocodeServer
 
 @dataclass
 class WebProxy(abc.ABC):
+    base_path: str
     server: HydrocodeServer = field(default_factory=HydrocodeServer, repr=False)
+
+    @abc.abstractmethod
+    def new_site_link(self, name: str, domain: Optional[str] = None, config_path: Optional[str] = None) -> None:
+        pass
+
+    @abc.abstractmethod
+    def remove_site_link(self, name: str, remove_config: bool = False) -> None:
+        pass
+
+    @abc.abstractmethod
+    def reload(self) -> None:
+        pass
+
